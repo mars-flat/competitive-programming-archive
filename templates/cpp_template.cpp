fc3ff721e6975c2b7efe3cdd6ab0b3c0c2e3f46b
Shane Chen <75336695+mars-flat@users.noreply.github.com>
2022-05-13 01:17:42 -0400

    Create cpp_template.cpp

---
+++ templates/cpp_template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define boost() ios_base::sync_with_stdio(0); cin.tie(0);
+#define pii pair<int, int>
+#define pc(x) "Case #" << x << ": "
+
+using namespace std;
+
+void solve(int case_num) {
+
+}
+
+int main() {
+    boost();
+    int t; cin >> t;
+    for (int i = 1; i <= t; ++i) solve(i);
+}
